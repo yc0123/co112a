0c6455170a82d52897253e6d872dd2efabba1014
yc0123 <s111110507@student.nqu.edu.tw>
2023-11-15 15:16:21 +0800

    jfyg

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2
+M=0
+@R0
+D=M
+@end
+D;JLE
+@R1
+D=M
+@end
+D;JLE
+
+(loop)
+@R1
+D=M
+@R2
+M=M+D
+@R0
+M=M-1
+D=M
+@end
+D;JLE
+@loop
+0;JMP
+
+(end)
+@end
+0;JMP
